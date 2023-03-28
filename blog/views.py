4dbb97f551d30b9b53a5d0427b6b2d75caae68b2
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-03-29 02:53:47 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,6 @@ from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
     
     return {
@@ -17,15 +13,15 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
+        'tags': [tag.title for tag in post.tags.all()],
+        'first_tag_title': post.tags.all().first().title,
     }
 
 
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts_count,
     }
 
 
@@ -39,11 +35,17 @@ def index(request):
 
     fresh_posts = Post.objects\
         .year(2018)\
-            .prefetch_related('author')\
-                .fetch_with_comments_count()
+            .prefetch_related('tags')\
+                .prefetch_related('author')\
+                    .fetch_with_comments_count()
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = Tag.objects\
+        .prefetch_related('posts')\
+            .annotate(posts_count=Count('posts'))\
+                .order_by('-posts_count')\
+                    .popular()[:5]
+
 
     context = {
         'most_popular_posts': [
