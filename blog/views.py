acc64a615cf0dc44f9a609f0610b78e3668fe142
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-03-29 00:49:28 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -31,13 +31,16 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = Post.objects.popular()\
-        .prefetch_related('author')[:5]\
-            .fetch_with_comments_count()
+    most_popular_posts = Post.objects\
+        .prefetch_related('tags')\
+            .popular()\
+                .prefetch_related('author')[:5]\
+                    .fetch_with_comments_count()
 
-    fresh_posts = Post.objects.year(2018)\
-        .prefetch_related('author')\
-            .fetch_with_comments_count()
+    fresh_posts = Post.objects\
+        .year(2018)\
+            .prefetch_related('author')\
+                .fetch_with_comments_count()
     most_fresh_posts = list(fresh_posts)[-5:]
 
     most_popular_tags = Tag.objects.popular()[:5]
@@ -81,9 +84,10 @@ def post_detail(request, slug):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = Post.objects.popular()\
-        .prefetch_related('author')[:5]\
-            .fetch_with_comments_count()
+    most_popular_posts = Post.objects\
+        .popular()\
+            .prefetch_related('author')[:5]\
+                .fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
@@ -99,11 +103,13 @@ def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = Post.objects.popular()\
-        .prefetch_related('author')[:5]\
-            .fetch_with_comments_count()
+    most_popular_posts = Post.objects\
+        .popular()\
+            .prefetch_related('author')[:5]\
+                .fetch_with_comments_count()
 
-    related_posts = tag.posts.all()[:20].fetch_with_comments_count()
+    related_posts = tag.posts.all()[:20]\
+        .fetch_with_comments_count()
 
     context = {
         'tag': tag.title,
