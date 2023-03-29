873b013e886dbf572f7a4947fafe9e60157e5929
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-03-29 20:23:40 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -58,7 +58,7 @@ def index(request):
 def post_detail(request, slug):
 
     post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes')), slug=slug)
-    comments = post.post_comments.prefetch_related('author')
+    comments = post.post_comments.select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
